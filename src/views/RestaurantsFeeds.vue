e5fa2cccc4e5168dc7a6a7481d01d677b43e393f
AllenliIE <allen.li.service@gmail.com>
2023-02-14 23:55:21 +0800

    feat: use filters and moment to change render view page

--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -13,6 +13,7 @@
       <div class="col-md-6">
         <!-- 最新評論 NewestComments-->
         <h3>最新評論</h3>
+        <NewestComments :comments="comments" />
       </div>
     </div>
   </div>
@@ -21,6 +22,7 @@
 <script>
 import NavTabs from "./../components/NavTabs";
 import NewestRestaurants from "./../components/NewestRestaurants";
+import NewestComments from "./../components/NewestComments";
 
 const dummyData = {
   restaurants: [
@@ -575,6 +577,7 @@ export default {
   components: {
     NavTabs,
     NewestRestaurants,
+    NewestComments,
   },
   data() {
     return {
@@ -587,8 +590,10 @@ export default {
   },
   methods: {
     fetchFeeds() {
-      this.restaurants = dummyData.restaurants;
-      this.comments = dummyData.comments;
+      const { restaurants, comments } = dummyData;
+      this.restaurants = restaurants;
+      this.comments = comments.filter((comment) => comment.Restaurant && comment.text
+      );
     },
   },
 };
